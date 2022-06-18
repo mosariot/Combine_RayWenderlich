3327f10b932cda759d2a3744aeb1739417113fb6
mosariot <mosariot@gmail.com>
2022-06-18 16:09:51 +0300

    Chapter 5 ends

--- Chapter 5/Chapter.playground/Contents.swift
+++ Chapter 5/Chapter.playground/Contents.swift
@@ -97,6 +97,145 @@ example(of: "append(Publisher)") {
         .store(in: &subscriptions)
 }
 
+// MARK: - Advanced
+
+example(of: "switchToLatest") {
+    let publisher1 = PassthroughSubject<Int, Never>()
+    let publisher2 = PassthroughSubject<Int, Never>()
+    let publisher3 = PassthroughSubject<Int, Never>()
+    
+    let publishers = PassthroughSubject<PassthroughSubject<Int, Never>, Never>()
+    
+    publishers
+        .switchToLatest()
+        .sink(
+            receiveCompletion: {_ in print("Completed!") },
+            receiveValue: { print($0)} )
+    
+    publishers.send(publisher1)
+    publisher1.send(1)
+    publisher1.send(2)
+    
+    publishers.send(publisher2)
+    publisher1.send(3)
+    publisher2.send(4)
+    publisher2.send(5)
+    
+    publishers.send(publisher3)
+    publisher2.send(6)
+    publisher3.send(7)
+    publisher3.send(8)
+    publisher3.send(9)
+    
+    publisher3.send(completion: .finished)
+    publishers.send(completion: .finished)
+}
+
+//example(of: "switchToLatest - Network Request") {
+//    let url = URL(string: "https://source.unsplash.com/random")!
+//
+//    func getImage() -> AnyPublisher<UIImage?, Never> {
+//        URLSession.shared
+//            .dataTaskPublisher(for: url)
+//            .map { data, _ in UIImage(data: data) }
+//            .print("image")
+//            .replaceError(with: nil)
+//            .eraseToAnyPublisher()
+//    }
+//
+//    let taps = PassthroughSubject<Void, Never>()
+//
+//    taps
+//        .map { _ in getImage() }
+//        .switchToLatest()
+//        .sink(receiveValue: { _ in })
+//        .store(in: &subscriptions)
+//
+//    taps.send()
+//
+//    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//        taps.send()
+//    }
+//
+//    DispatchQueue.main.asyncAfter(deadline: .now() + 3.1) {
+//        taps.send()
+//    }
+//}
+
+example(of: "merge(with:)") {
+    let publisher1 = PassthroughSubject<Int, Never>()
+    let publisher2 = PassthroughSubject<Int, Never>()
+    
+    publisher1
+        .merge(with: publisher2)
+        .sink(
+            receiveCompletion: { _ in print("Completed") },
+            receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    
+    publisher1.send(1)
+    publisher1.send(2)
+    
+    publisher2.send(3)
+    
+    publisher1.send(4)
+    
+    publisher2.send(5)
+    
+    publisher1.send(completion: .finished)
+    publisher1.send(completion: .finished)
+}
+
+example(of: "combineLatest") {
+    let publisher1 = PassthroughSubject<Int, Never>()
+    let publisher2 = PassthroughSubject<String, Never>()
+    
+    publisher1
+        .combineLatest(publisher2)
+        .sink(
+            receiveCompletion: { _ in print("Completed") },
+            receiveValue: { print("P1: \($0), P2: \($1)") }
+        )
+        .store(in: &subscriptions)
+    
+    publisher1.send(1)
+    publisher1.send(2)
+    
+    publisher2.send("a")
+    publisher2.send("b")
+    
+    publisher1.send(3)
+    
+    publisher2.send("c")
+    
+    publisher1.send(completion: .finished)
+    publisher2.send(completion: .finished)
+}
+
+example(of: "zip") {
+    let publisher1 = PassthroughSubject<Int, Never>()
+    let publisher2 = PassthroughSubject<String, Never>()
+    
+    publisher1
+        .zip(publisher2)
+        .sink(
+            receiveCompletion: { _ in print("Completed") },
+            receiveValue: { print("P1: \($0), P2: \($1)") }
+        )
+        .store(in: &subscriptions)
+    
+    publisher1.send(1)
+    publisher1.send(2)
+    publisher2.send("a")
+    publisher2.send("b")
+    publisher1.send(3)
+    publisher2.send("c")
+    publisher2.send("d")
+    
+    publisher1.send(completion: .finished)
+    publisher2.send(completion: .finished)
+}
+
 // Copyright (c) 2021 Razeware LLC
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy

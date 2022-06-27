732749b49eac403c90a9d6bc6f0052655d93d223
mosariot <mosariot@gmail.com>
2022-06-27 20:08:51 +0300

    Chapter 9

---
+++ Chapter 9/Chapter.playground/Contents.swift
@@ -0,0 +1,68 @@
+import Foundation
+import Combine
+
+var subscriptions = Set<AnyCancellable>()
+
+struct MyType: Codable { }
+
+example(of: "Networking") {
+    guard let url = URL(string: "https://mysite.com/mydata.json") else { return }
+    
+    let _ = URLSession.shared
+        .dataTaskPublisher(for: url)
+        .sink { completion in
+            if case .failure(let err) = completion {
+                print("Retrieving data faled with error \(err)")
+            }
+        } receiveValue: { data, response in
+            print("Retrieved data of size \(data.count), response = \(response)")
+        }
+}
+
+example(of: "Networking with decoding") {
+    guard let url = URL(string: "https://mysite.com/mydata.json") else { return }
+    
+    let _ = URLSession.shared
+        .dataTaskPublisher(for: url)
+        .map(\.data)
+        .decode(type: MyType.self, decoder: JSONDecoder())
+        .sink { completion in
+            if case .failure(let err) = completion {
+                print("Retrieving data faled with error \(err)")
+            }
+        } receiveValue: { object in
+            print("Retrieved object \(object)")
+        }
+}
+
+example(of: "Connecting multiple subscribers to the networking publiher") {
+    guard let url = URL(string: "https://mysite.com/mydata.json") else { return }
+    
+    let publisher = URLSession.shared
+        .dataTaskPublisher(for: url)
+        .map(\.data)
+        .multicast {
+            PassthroughSubject<Data, URLError>()
+        }
+    
+    let _ = publisher
+        .sink { completion in
+            if case .failure(let err) = completion {
+                print("Sink1 Retrieving data faled with error \(err)")
+            }
+        } receiveValue: { object in
+            print("Sink1 Retrieved object \(object)")
+        }
+    
+    let _ = publisher
+        .sink { completion in
+            if case .failure(let err) = completion {
+                print("Sink2 Retrieving data faled with error \(err)")
+            }
+        } receiveValue: { object in
+            print("Sink2 Retrieved object \(object)")
+        }
+    
+    let _ = publisher
+        .connect()
+}

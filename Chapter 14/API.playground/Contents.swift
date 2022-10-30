3b97d3fc2e0087623fbd010532959e1b93478f7b
mosariot <mosariot@gmail.com>
2022-10-30 19:14:04 +0300

    Chapter 14 in the middle

--- Chapter 14/API.playground/Contents.swift
+++ Chapter 14/API.playground/Contents.swift
@@ -3,47 +3,77 @@ import PlaygroundSupport
 import Combine
 
 struct API {
-  /// API Errors.
-  enum Error: LocalizedError {
-    case addressUnreachable(URL)
-    case invalidResponse
-    
-    var errorDescription: String? {
-      switch self {
-      case .invalidResponse: return "The server responded with garbage."
-      case .addressUnreachable(let url): return "\(url.absoluteString) is unreachable."
-      }
+    /// API Errors.
+    enum Error: LocalizedError {
+        case addressUnreachable(URL)
+        case invalidResponse
+        
+        var errorDescription: String? {
+            switch self {
+            case .invalidResponse: return "The server responded with garbage."
+            case .addressUnreachable(let url): return "\(url.absoluteString) is unreachable."
+            }
+        }
     }
-  }
-  
-  /// API endpoints.
-  enum EndPoint {
-    static let baseURL = URL(string: "https://hacker-news.firebaseio.com/v0/")!
     
-    case stories
-    case story(Int)
-    
-    var url: URL {
-      switch self {
-      case .stories:
-        return EndPoint.baseURL.appendingPathComponent("newstories.json")
-      case .story(let id):
-        return EndPoint.baseURL.appendingPathComponent("item/\(id).json")
-      }
+    /// API endpoints.
+    enum EndPoint {
+        static let baseURL = URL(string: "https://hacker-news.firebaseio.com/v0/")!
+        
+        case stories
+        case story(Int)
+        
+        var url: URL {
+            switch self {
+            case .stories:
+                return EndPoint.baseURL.appendingPathComponent("newstories.json")
+            case .story(let id):
+                return EndPoint.baseURL.appendingPathComponent("item/\(id).json")
+            }
+        }
     }
-  }
-
-  /// Maximum number of stories to fetch (reduce for lower API strain during development).
-  var maxStories = 10
-
-  /// A shared JSON decoder to use in calls.
-  private let decoder = JSONDecoder()
-  
-  // <#Add your API code here#>
-  
+    
+    /// Maximum number of stories to fetch (reduce for lower API strain during development).
+    var maxStories = 10
+    
+    /// A shared JSON decoder to use in calls.
+    private let decoder = JSONDecoder()
+    
+    private let apiQueue = DispatchQueue(label: "API", qos: .default, attributes: .concurrent)
+    
+    // <#Add your API code here#>
+    func story(id: Int) -> AnyPublisher<Story, Error> {
+        URLSession.shared
+            .dataTaskPublisher(for: EndPoint.story(id).url)
+            .receive(on: apiQueue)
+            .map(\.data)
+            .decode(type: Story.self, decoder: decoder)
+            .catch { _ in Empty<Story, Error>() }
+            .eraseToAnyPublisher()
+    }
+    
+    func mergedStories(ids storyIDs: [Int]) -> AnyPublisher<Story, Error> {
+        let storyIDs = Array(storyIDs.prefix(maxStories))
+        precondition(!storyIDs.isEmpty)
+        let initialPublisher = story(id: storyIDs[0])
+        let remainder = Array(storyIDs.dropFirst())
+        return remainder.reduce(initialPublisher) { combined, id in
+            combined
+                .merge(with: story(id: id))
+                .eraseToAnyPublisher()
+        }
+    }
 }
 
 // <#Call the API here#>
+let api = API()
+var subscriptions = [AnyCancellable]()
+//api.story(id: -5)
+//    .sink(receiveCompletion: { print($0) }, receiveValue: { print($0) })
+//    .store(in: &subscriptions)
+api.mergedStories(ids: [1000, 1001, 1002])
+    .sink(receiveCompletion: { print($0) }, receiveValue: { print($0) })
+    .store(in: &subscriptions)
 
 
 // Run indefinitely.

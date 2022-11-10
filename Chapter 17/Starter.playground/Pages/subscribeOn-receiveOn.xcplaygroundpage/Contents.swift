b9731532198c51bde475baf2b027876377dab204
mosariot <mosariot@gmail.com>
2022-11-10 10:29:05 +0300

    Chapter 17. subscribeOn_receiveOn

--- Chapter 17/Starter.playground/Pages/subscribeOn-receiveOn.xcplaygroundpage/Contents.swift
+++ Chapter 17/Starter.playground/Pages/subscribeOn-receiveOn.xcplaygroundpage/Contents.swift
@@ -1,7 +1,17 @@
 import Foundation
 import Combine
 
-<# Add code here #>
+let computationPublisher = Publishers.ExpensiveComputation(duration: 3)
+let queue = DispatchQueue(label: "serial queue")
+let currentThread = Thread.current.number
+print("Start computation publisher on thread \(currentThread)")
+let subscription = computationPublisher
+    .subscribe(on: queue)
+    .receive(on: DispatchQueue.main)
+    .sink { value in
+        let thread = Thread.current.number
+        print("Received computation result on thread \(thread): '\(value)'")
+    }
 
 //: [Next](@next)
 /*:

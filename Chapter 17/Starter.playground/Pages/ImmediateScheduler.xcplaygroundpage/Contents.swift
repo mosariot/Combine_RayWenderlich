91b380bf2b0514ffbfc3b79f4106cf66931a6e09
mosariot <mosariot@gmail.com>
2022-11-11 09:56:03 +0300

    Chapter 17. ImmediateScheduler

--- Chapter 17/Starter.playground/Pages/ImmediateScheduler.xcplaygroundpage/Contents.swift
+++ Chapter 17/Starter.playground/Pages/ImmediateScheduler.xcplaygroundpage/Contents.swift
@@ -2,7 +2,22 @@ import Combine
 import SwiftUI
 import PlaygroundSupport
 
-<# Add code here #>
+let source = Timer
+    .publish(every: 1.0, on: .main, in: .common)
+    .autoconnect()
+    .scan(0) { counter, _ in counter + 1 }
+
+let setupPublisher = { recorder in
+    source
+        .receive(on: DispatchQueue.global())
+        .recordThread(using: recorder)
+        .receive(on: ImmediateScheduler.shared)
+        .recordThread(using: recorder)
+        .eraseToAnyPublisher()
+}
+
+let view = ThreadRecorderView(title: "Using ImmediateScheduler", setup: setupPublisher)
+PlaygroundPage.current.liveView = UIHostingController(rootView: view)
 
 //: [Next](@next)
 /*:

59de95455887e27a1c63cb00736eb52d025a0133
mosariot <mosariot@gmail.com>
2022-11-16 10:12:52 +0300

    Chapter 18. Publisher extension

--- Chapter 18/Starter.playground/Pages/Unwrap operator.xcplaygroundpage/Contents.swift
+++ Chapter 18/Starter.playground/Pages/Unwrap operator.xcplaygroundpage/Contents.swift
@@ -1,6 +1,17 @@
 import Combine
 
-<#Add your code here#>
+extension Publisher {
+    func unwrap<T>() -> Publishers.CompactMap<Self, T> where Output == Optional<T> {
+        compactMap { $0 }
+    }
+}
+
+let values: [Int?] = [1, 2, nil, 3, nil, 4]
+values.publisher
+    .unwrap()
+    .sink {
+        print("Received value: \($0)")
+    }
 
 //: [Next](@next)
 /*:
